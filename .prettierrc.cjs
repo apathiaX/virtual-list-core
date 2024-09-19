eb3f8ab5179ac390e2af7b91ba384d3e99cba778
apathiaX <xuzhicheng122@163.com>
2024-09-20 01:34:56 +0800

    wip: add husky

--- .prettierrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  singleQuote: true, // 单引号优先
-  semi: true, // 是否加分号
-  trailingComma: 'all', // 末尾加逗号 默认none es5 包括es5中的数组、对象 all 包括函数对象等所有可选
-  arrowParens: 'always',
-  wrapAttributes: true,
-  sortAttributes: true, // 属性自动排序
-};
